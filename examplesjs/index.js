504cc58f491beed9ef2b10c8cd10886b7c67a75c
mramoss <moi6ramoss@gmail.com>
2019-06-12 11:06:27 -0400

    Data by value or by reference

---
+++ examplesjs/index.js
@@ -0,0 +1,26 @@
+const a = {
+    name: 'Moises',
+    lastName: 'Ramos'
+}
+
+console.log(a.name);
+a.name = 'Jorge';
+console.log(a);
+
+
+/* arrreglo por referencia
+let nos permite reasignar
+variable 
+objetos y arreglos por referenias
+gasbar colector --> Leeer
+con el tiempo cambio de tripado
+pasar por valor los primer types
+*/
+
+
+/*construccion a un objeto por referencia*/
+
+/*entero por copia */
+/* */
+z=3;
+y = "Hello world";
